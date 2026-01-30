f7cf1b2e643c83f79c4e7b5bc217db53817f605d
patrick.petre <patrickpetre823@yahoo.de>
2026-01-30 17:50:25 +0100

    added api key, gitignore

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -2,18 +2,26 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 
-def hello_world():
-    print("Hello, World!")
+def get_data():
+    url = "https://creativecommons.tankerkoenig..de/api/v4"
+    response = requests.get(url)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        return response.json() 
+    else:
+        return f"Fehler: {response.status_code}"
+    
 
 
 with DAG(
     "my_dag", 
     start_date=datetime(2025, 1, 25),
-    schedule="@daily",
+    schedule="* * * * 5",
     catchup=False
 ):
     PythonOperator(
         task_id="my_task",
-        python_callable=hello_world
+        python_callable=get_data
         )
     
