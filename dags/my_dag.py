85360d5e251c38817c3aa45cd5eb77e8629df177
patrick.petre <patrickpetre823@yahoo.de>
2026-02-08 19:36:37 +0100

    Added Xcom and consolidated tasks

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -12,36 +12,45 @@ load_dotenv("../.env")  # Lädt die .env-Datei
 api_key = os.getenv("TANKERKOENIG_API_KEY")
 print("API-Key geladen:", api_key is not None)  # Sollte True sein
 
-def get_data():
+def get_data(**context):
+
+    ti = context['ti']       # Task instance um XCOM zu verwenden
     
     url = f"https://creativecommons.tankerkoenig.de/json/list.php?lat=48.775&lng=9.172&rad=10&sort=dist&type=all&apikey={api_key}"
     response = requests.get(url)
 
     # Check if request was successful
     if response.status_code == 200:
-        print("Daten erfolgreich abgerufen")
+        ti.xcom_push(key="api_response", value=response.json())  # Speichert die API-Antwort in XCom
         return response.json() 
     else:
         return f"Fehler: {response.status_code}"
     
-def make_df():
 
-    data = get_data()
-    tankstellen = data["stations"]
+def make_df(**context):
+
+    ti  = context['ti']  # Task instance um XCOM zu verwenden
+    data = ti.xcom_pull(key="api_response", task_ids="get_data")  # Holt die API-Antwort aus XCom
+
+    if not data:
+        print("Keine Daten gefunden in XCom")
+        return None
+
+    tankstellen = data["tankstellen.db"]
     df = pd.DataFrame(tankstellen)
 
     df["retrieval_time"] = datetime.now(pytz.timezone("Europe/Berlin")).strftime("%H:%M")
     df["retrieval_date"] = datetime.now().date()
 
-    return df
-
-def write_db():
+    # Write to database
     conn = sqlite3.connect("tankstellen.db")
-    df = make_df()
     df.to_sql("tankstellen", conn, if_exists="append", index=False)
     historische_daten = pd.read_sql('SELECT * FROM tankstellen', conn)
     print(historische_daten)
     conn.close()
+
+    return df
+
 
 
 with DAG(
@@ -59,8 +68,5 @@ with DAG(
         task_id="make_df",
         python_callable=make_df
         )
-    task_write_db = PythonOperator(
-        task_id="write_db",
-        python_callable=write_db
-        )
-    task_get_data >> task_make_df >> task_write_db
+
+    task_get_data >> task_make_df 
